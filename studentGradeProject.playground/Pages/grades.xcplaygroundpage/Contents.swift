e2bbd5e9e00e200f5ff22cec1b9f85d7b8f2fbc1
ppvq11 <muai046@gmail.com>
2024-08-01 20:29:19 +0300

    Initial Commit

---
+++ studentGradeProject.playground/Pages/grades.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+//: [Previous](@previous)
+
+
+import UIKit
+
+var grade = 85
+
+if (grade >= 90){
+    print("Exellent")
+}
+else if (grade >= 80){
+    print("Very good")
+}
+else if (grade >= 70){
+    print("Good")
+}
+else if (grade >= 60){
+    print("Pass")
+}
+else{
+    print("Fail")
+}
