078d0a88a91af50163879377e461d19f31725ccc
ppvq11 <muai046@gmail.com>
2024-08-04 19:47:55 +0300

    switch

---
+++ studentGradeProject.playground/Pages/gradesWithSwitch.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+//: [Previous](@previous)
+
+let studentGrade = 77
+
+switch studentGrade {
+case 90...100 :
+    print("Excellent")
+    
+case 80...89 :
+    print("Very good")
+    
+case 70...79:
+    print("Good")
+    
+case 60...69:
+    print("Pass")
+    
+default:
+    print("Fail")
+    
+}
